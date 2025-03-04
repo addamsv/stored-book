0be04e23ad4e85f2b0af60c5ce847b1c0a6c49c9
addamsv <googolcalculator@gmail.com>
2025-03-04 07:57:45 +0300

    add help images

--- src/pages/Help/ui/Help.tsx
+++ src/pages/Help/ui/Help.tsx
@@ -15,6 +15,19 @@ const Help = () => {
         <VFlex gap="8">
           <Text textAlign={TextAlign.LEFT} textSize={TextSize.L} title="FAQ" />
           <Text textAlign={TextAlign.LEFT} text="How to download a link?" />
+          <HFlex className={cls.flexWrap} gap="16">
+            <img width={280} height={480} src="/images/step_1.jpg" alt="step 1" />
+            <img width={280} height={480} src="/images/step_2.jpg" alt="step 2" />
+            <img width={280} height={480} src="/images/step_3.jpg" alt="step 3" />
+            <img width={280} height={480} src="/images/step_4.jpg" alt="step 4" />
+            <img width={280} height={480} src="/images/step_5.jpg" alt="step 5" />
+            <img width={280} height={480} src="/images/step_6.jpg" alt="step 6" />
+            <img width={280} height={480} src="/images/step_7.jpg" alt="step 7" />
+            <img width={280} height={480} src="/images/step_8.jpg" alt="step 8" />
+            <img width={280} height={480} src="/images/step_9.jpg" alt="step 9" />
+            <img width={280} height={480} src="/images/step_10.jpg" alt="step 10" />
+            <img width={280} height={480} src="/images/step_11.jpg" alt="step 11" />
+          </HFlex>
         </VFlex>
       </Card>
     </Page>
