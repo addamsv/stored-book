d6b54099ddbc988bffd5ee9c7b941b792bba0a22
addamsv <googolcalculator@gmail.com>
2025-04-24 15:31:48 +0300

    ref: help list on the server side

--- src/pages/BookDetailsPage/ui/BookDetailsPage/BookDetailsPage.tsx
+++ src/pages/BookDetailsPage/ui/BookDetailsPage/BookDetailsPage.tsx
@@ -1,26 +1,13 @@
 import { classes } from "resources/lib/classNames/classes";
 import { useTranslation } from "react-i18next";
-import { memo, useCallback, useEffect } from "react";
-import { BookDetails, BookList, EBookListView } from "entities/Book";
+import { memo } from "react";
+import { BookDetails } from "entities/Book";
 import { useParams } from "react-router-dom";
-import { Text } from "shared/Text/Text";
-import { TextSize } from "shared/Text";
-import { CommentList } from "entities/Comment";
 import { AsyncModule, ReducerListT } from "shared/AsyncModule/AsyncModule";
-import { useSelector } from "react-redux";
-import { useAppDispatch } from "resources/hooks/useAppDispatch";
-import { SendCommentForm } from "features/SendCommentForm";
 import { Page } from "widgets/Page/Page";
-import { getUserAuthData } from "entities/User";
 import { BookBottomNavbar } from "widgets/BookBottomNavbar";
 import { BookRecommendationsList } from "features/BookRecommendationsList";
-import { bookDetailsCommentsReducer, getBooksComments } from "../../model/slices/bookDetailsCommentsSlice";
 import cls from "./BookDetailsPage.module.scss";
-import { getBooksCommentsError, getBooksCommentsIsLoading } from "../../model/selectors";
-import { fetchCommentsByBookId, sendBookComment } from "../../model/services";
-import { getRecommendations, recommendationsReducer } from "../../model/slices/recommendationSlice";
-import { getBooksRecommendationsError, getBooksRecommendationsIsLoading } from "../../model/selectors/recommendations";
-import { fetchRecommendations } from "../../model/services/fetchRecommendations";
 import { bookDetailsPageReducer } from "../../model/slices";
 import { BookDetailsHeader } from "../BookDetailsHeader/BookDetailsHeader";
 import { BookDetailsComments } from "../BookDetailsComments/BookDetailsComments";
@@ -30,9 +17,7 @@ interface IBookDetailsPageProps {
 }
 
 const reducerList: ReducerListT = {
-  bookDetailsPage: bookDetailsPageReducer,
-  // bookDetailsComments: bookDetailsCommentsReducer,
-  // bookDetailsRecommendations: recommendationsReducer
+  bookDetailsPage: bookDetailsPageReducer
 };
 
 const BookDetailsPage = ({ className }: IBookDetailsPageProps) => {
