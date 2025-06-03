fc2e932bac2c3e97d016b2db8f7ce832b7073cd3
addamsv <googolcalculator@gmail.com>
2025-06-03 12:56:23 +0300

    ref: minor upd

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "app/App";
 import { ThemeProvider } from "resources/store/ThemeProvider";
@@ -6,8 +5,17 @@ import "resources/config/i18n/i18n";
 import { ErrorBoundary } from "shared/Error";
 import { StoreProvider } from "resources/store/StoreProvider";
 import { ErrorWidget } from "widgets/Error";
+import { createRoot } from "react-dom/client";
 
-render(
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Could not found root container");
+}
+
+const root = createRoot(container);
+
+root.render(
   <StoreProvider>
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <ErrorBoundary WidgetOfError={ErrorWidget}>
@@ -16,7 +24,5 @@ render(
         </ThemeProvider>
       </ErrorBoundary>
     </BrowserRouter>
-  </StoreProvider>,
-
-  document.getElementById("root")
+  </StoreProvider>
 );
